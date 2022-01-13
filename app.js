3edc27f420e642e4436e4207dc8d110d727b1481
algo-ryth-nic <praanjjal.agnihotri@gmail.com>
2022-01-13 16:40:43 +0530

    changed record -> recordAudio variable name, minor change

--- app.js
+++ app.js
@@ -11,72 +11,78 @@ if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
 
 
 // Global constants
-const constraints = {audio: true, video: false};
+// const constraints = {, video: true};
 let audioChunks = [];
+let videoChunks = [];
 
 let stream = null;
 let mediaRecorder = null;
 
-record = document.getElementById('recordButton');
+recordAudio = document.getElementById('recordButton');
 
-record.onclick = async function(event){
+recordAudio.onclick = async function(event){
     if (!stream){
-        stream = await navigator.mediaDevices.getUserMedia(constraints);
+        stream = await navigator.mediaDevices.getUserMedia({audio: true});
     }
 
-    microphone_status = record.firstElementChild.style.color;
+    // when stream is still active
+    if(stream.active){
+        microphone_status = record.firstElementChild.style.color;
     
-    // is recording
-    if(microphone_status == "red"){
-        document.getElementById("recordButton").firstElementChild.style.color = "black";
-        mediaRecorder.stop();            
-    }
-    
-    // not recording
-    else{
-        document.getElementById("recordButton").firstElementChild.style.color = "red";
+        // is recording
+        if(microphone_status == "red"){
+            document.getElementById("recordButton").firstElementChild.style.color = "black";
+            mediaRecorder.stop();            
+        }
         
-        mediaRecorder = new MediaRecorder(stream);
-        
-        // event handling
-        // stores the audio chunks (bytes) in audioChunks. Dataavailable event is raise when .stop() is called.
-        mediaRecorder.addEventListener('dataavailable', function(event){
-            audioChunks.push(event.data);
-        });
-
-        // event handling of 'stop'
-        mediaRecorder.onstop = function(event){
-            console.log("making data available after MediaRecorder.stop() called.");
-
-            // when audio recording is stopped, save recording chunks in <audio> for playing 
-            let audio = document.createElement('audio');
-            audio.controls = true;
+        // start recording
+        else{
+            document.getElementById("recordButton").firstElementChild.style.color = "red";
             
-            // creating a single blob from the chunks
-            const blob = new Blob(audioChunks, {'type' : 'audio/ogg; codecs=opus'});
-            audioChunks = [];
-
-            // creating a object url for download and playing in player
-            const audioURL = URL.createObjectURL(blob);
-
-            audio.src = audioURL;
-            audio.classList = "p-2";
-            audio.style.borderRadius = "5px";
-
-            document.getElementsByClassName('audio-container')[0].appendChild(audio);
-        }
-
-
-        // reset audio player
-        for (audioPlayer of document.getElementsByClassName('audio-container')[0].children){
-            if (audioPlayer){
-                audioPlayer.pause();
-                audioPlayer.currentTime = 0;
-            }
-        }
-        mediaRecorder.start();
-    }
-
-    console.log(mediaRecorder.state);
+            mediaRecorder = new MediaRecorder(stream);
+            
+            // event handling
+            // stores the audio chunks (bytes) in audioChunks. Dataavailable event is raise when .stop() is called.
+            mediaRecorder.addEventListener('dataavailable', function(event){
+                audioChunks.push(event.data);
+            });
     
+            // event handling of 'stop'
+            mediaRecorder.onstop = function(event){
+                console.log("making data available after MediaRecorder.stop() called.");
+    
+                // when audio recording is stopped, save recording chunks in <audio> for playing 
+                let audio = document.createElement('audio');
+                audio.controls = true;
+                
+                // creating a single blob from the chunks
+                const blob = new Blob(audioChunks, {'type' : 'audio/ogg; codecs=opus'});
+                audioChunks = [];
+    
+                // creating a object url for download and playing in player
+                const audioURL = URL.createObjectURL(blob);
+    
+                audio.src = audioURL;
+                audio.classList = "p-2";
+                audio.style.borderRadius = "5px";
+    
+                document.getElementsByClassName('audio-container')[0].appendChild(audio);
+            }
+    
+    
+            // reset audio player
+            for (audioPlayer of document.getElementsByClassName('audio-container')[0].children){
+                if (audioPlayer){
+                    audioPlayer.pause();
+                    audioPlayer.currentTime = 0;
+                }
+            }
+            mediaRecorder.start();
+        }
+    
+        console.log(mediaRecorder.state);
+    }
 }
+
+
+// for video 
